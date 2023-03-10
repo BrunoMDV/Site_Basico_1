74743909d8141a0f29c9e8e7de844b239ab1ac95
BrunoMDV <72626913+BrunoMDV@users.noreply.github.com>
2023-03-10 19:45:39 -0300

    cookie-done

--- mern-exercise-tracker/backend/routes/authRoutes.js
+++ mern-exercise-tracker/backend/routes/authRoutes.js
@@ -6,4 +6,8 @@ module.exports = (app) => {
 		passport.authenticate("google", { scope: ["profile", "email"] })
 	);
 	app.get("/auth/google/callback", passport.authenticate("google"));
+
+	app.get("/api/current_user", (req, res) => {
+		res.send(req.user);
+	});
 };
--- mern-exercise-tracker/backend/services/passport.js
+++ mern-exercise-tracker/backend/services/passport.js
@@ -1,7 +1,18 @@
 const passport = require("passport");
-const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then((user) => {
+		done(null, user);
+	});
+});
 passport.use(
 	new GoogleStrategy(
 		{
@@ -9,8 +20,18 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: "/auth/google/callback",
 		},
-		(accessToken) => {
-			console.log(accessToken);
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					// we already have a record with the given profile ID
+					done(null, existingUser);
+				} else {
+					// we don't have a user record with this ID, make a new record!
+					new User({ googleId: profile.id })
+						.save()
+						.then((user) => done(null, user));
+				}
+			});
 		}
 	)
 );
