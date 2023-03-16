601ffa00728d90bdbc2b11ae71463e73a3ed0f65
BrunoMDV <72626913+BrunoMDV@users.noreply.github.com>
2023-03-16 16:08:06 -0300

    oauth_test_railway

--- mern-exercise-tracker/backend/routes/authRoutes.js
+++ mern-exercise-tracker/backend/routes/authRoutes.js
@@ -5,7 +5,13 @@ module.exports = (app) => {
 		"/auth/google",
 		passport.authenticate("google", { scope: ["profile", "email"] })
 	);
-	app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get(
+		"/auth/google/callback",
+		passport.authenticate("google"),
+		(req, res) => {
+			res.redirect("/surveys");
+		}
+	);
 
 	app.get("/api/logout", (req, res) => {
 		req.logout();
